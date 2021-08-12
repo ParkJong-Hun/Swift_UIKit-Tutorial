678d4e9c469348900353d45b5a15bc12bf22b301
ParkJong-Hun <thesn515@gmail.com>
2021-08-12 19:02:23 +0900

    2021/08/12 19:02 Simple + - Calculator

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -6,33 +6,21 @@
 //
 
 import UIKit
-import WebKit
 
 class ViewController: UIViewController {
-    /*
-     WebKitView = 웹뷰.
-     ctrl + 클릭 드래그를 이용해 뷰 컨트롤러에 WKWebView를 생성 (WebKit import 필요!)
-     */
-    //웹뷰에 웹사이트 출력하는 법
-    @IBOutlet weak var WebViewMain: WKWebView!
+    //버튼 클릭하면 값 오르게 하기
+    @IBOutlet weak var Number: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //5. 만든 웹뷰 로딩 함수 호출
-        urlPass()
-    }
-    //1. 웹뷰 로딩 함수 정의
-    func urlPass() {
-        //2. 주소를 String으로 받아서 URL 형식으로 변환
-        let urlStr = "https://www.google.com/"
-        let url = URL(string: urlStr)
-        //3. URL을 URLrequest로 바꿈
-        let urlReq = URLRequest(url: url!)
-        //4. request를 load함
-        WebViewMain.load(urlReq)
     }
     
+    @IBAction func UpEvent(_ sender: Any) {
+        Number.text = "\(Int(Number.text!)! + 1)"
+    }
     
-    
+    @IBAction func DownEvent(_ sender: Any) {
+        Number.text = "\(Int(Number.text!)! - 1)"
+    }
 }
 
