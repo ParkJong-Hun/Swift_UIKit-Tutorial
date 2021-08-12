3e30f11c1c469a64bc7d85dd6b96e285893fec76
ParkJong-Hun <thesn515@gmail.com>
2021-08-12 14:26:45 +0900

    2021/08/12 14:26 Create Button View

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    /*
+    오른쪽 상단의 + 버튼을 클릭해 view를 storyboard에 드래그해 생성할 수 있음
+    automatic asistant는 class가 잘 연결되어 있어야 가능
+    stroyboard 밑에 두번째, 세번째거 클릭해서 Constraint 지정 가능
+     */
+    
+    //storyboard에서 뷰를 Ctrl + 클릭하여 드래그하면 이렇게 이벤트 처리 코드를 생성할 수 있음
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        print("moved!")
+    }
+    
 }
 
