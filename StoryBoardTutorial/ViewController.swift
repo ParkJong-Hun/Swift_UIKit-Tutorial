acab943978e58b45aaaa4194dbe58825780309d1
ParkJong-Hun <thesn515@gmail.com>
2021-08-13 15:28:06 +0900

    2021/15:27 Use Switch

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -8,18 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //문장을 입력하면 다음 화면에 문장 표시
-    @IBOutlet weak var Input: UITextField!
-    @IBAction func NextBtn(_ sender: Any) {
-        guard let controller = self.storyboard?.instantiateViewController(identifier: "ResultController") as ResultController? else {
-            return
-        }
-        controller.label = Input.text ?? "nil"
-        navigationController?.pushViewController(controller, animated: true)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    var price = 0
+    @IBOutlet weak var Button_Price: UILabel!
+    @IBAction func Switch_Pizza(_ sender: UISwitch) {
+        if sender.isOn {
+            price += 15
+            Button_Price.text = "$\(price)"
+        } else {
+            price -= 15
+            if price < 0 {
+                price = 0
+            }
+            Button_Price.text = "$\(price)"
+        }
+    }
+    @IBAction func Switch_Coke(_ sender: UISwitch) {
+        if sender.isOn {
+            price += 2
+            Button_Price.text = "$\(price)"
+        } else {
+            price -= 2
+            if price < 0 {
+                price = 0
+            }
+            Button_Price.text = "$\(price)"
+        }
+    }
+    @IBAction func Switch_Hamburger(_ sender: UISwitch) {
+        if sender.isOn {
+            price += 6
+            Button_Price.text = "$\(price)"
+        } else {
+            price -= 6
+            if price < 0 {
+                price = 0
+            }
+            Button_Price.text = "$\(price)"
+        }
     }
 }
 
