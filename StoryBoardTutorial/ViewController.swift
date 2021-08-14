7b8eca829911db5710c7b35bb3f1b2d38f8ea3b6
ParkJong-Hun <thesn515@gmail.com>
2021-08-14 14:59:53 +0900

    2021/08/14 14:59 Create Alert

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -11,43 +11,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    var price = 0
-    @IBOutlet weak var Button_Price: UILabel!
-    @IBAction func Switch_Pizza(_ sender: UISwitch) {
-        if sender.isOn {
-            price += 15
-            Button_Price.text = "$\(price)"
-        } else {
-            price -= 15
-            if price < 0 {
-                price = 0
-            }
-            Button_Price.text = "$\(price)"
-        }
-    }
-    @IBAction func Switch_Coke(_ sender: UISwitch) {
-        if sender.isOn {
-            price += 2
-            Button_Price.text = "$\(price)"
-        } else {
-            price -= 2
-            if price < 0 {
-                price = 0
-            }
-            Button_Price.text = "$\(price)"
-        }
-    }
-    @IBAction func Switch_Hamburger(_ sender: UISwitch) {
-        if sender.isOn {
-            price += 6
-            Button_Price.text = "$\(price)"
-        } else {
-            price -= 6
-            if price < 0 {
-                price = 0
-            }
-            Button_Price.text = "$\(price)"
-        }
+    @IBAction func showAlert(_ sender: Any) {
+        let uialert = UIAlertController(title: "AlertTitle", message: "This is Alert message article!", preferredStyle: UIAlertController.Style.alert)
+        uialert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: nil))
+        self.present(uialert, animated: true, completion: nil)
     }
 }
 
