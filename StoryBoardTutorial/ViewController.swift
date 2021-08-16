57d83722d8fe7f63de13e256e793671c251786ed
ParkJong-Hun <thesn515@gmail.com>
2021-08-16 09:04:00 +0900

    2021/09:03 Activity Indicator

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -11,10 +11,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func showAlert(_ sender: Any) {
-        let uialert = UIAlertController(title: "AlertTitle", message: "This is Alert message article!", preferredStyle: UIAlertController.Style.alert)
-        uialert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: nil))
-        self.present(uialert, animated: true, completion: nil)
-    }
 }
 
