b63ce2eadf6d3eb57df092a2788c91dffa689558
ParkJong-Hun <thesn515@gmail.com>
2021-08-17 16:20:43 +0900

    Create Custom Collection View

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -7,16 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        return cell
-    }
+class ViewController: UIViewController {
+    var cellName = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+}
+//커스텀 셀
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
+
+//컬렉션 뷰
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellName.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        cell.backgroundColor = UIColor.lightGray
+        cell.label.text = String(cellName[indexPath.row])
+        cell.label.backgroundColor = UIColor.white
+        return cell
+    }
+}
+//셀 레이아웃
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    //위아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+    }
+    //옆 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+    }
+    //셀 사이즈
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let width = collectionView.frame.width / 3 - 1
+            let size = CGSize(width: width, height: width)
+            return size
     }
 }
