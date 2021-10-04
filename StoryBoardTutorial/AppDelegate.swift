3fc18809c68b1dfcfe8e5c0f6701216b592b3d8f
ParkJong-Hun <thesn515@gmail.com>
2021-10-04 17:04:42 +0900

    2021/10/04 17:04 Push Notifications with FirebaseMessaging

--- StoryBoardTutorial/AppDelegate.swift
+++ StoryBoardTutorial/AppDelegate.swift
@@ -6,15 +6,45 @@
 //
 
 import UIKit
+//푸쉬 알림을 위한 임포트
+import UserNotifications
+import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        //푸쉬 알림을 생성하기위해 현재 알림센터에 (알림, 소리, 뱃지) 권한 요청
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { (success, error) in
+            guard success else {
+                //APNs(Apple push norification service) 에러
+                print("APNs Registry 에러: \(error.debugDescription)")
+                return
+            }
+            //APNS 성공
+            print("APNs Registry 성공 \(success.description)")
+        })
+        //앱에 원격 알림을 등록
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    //메시지 출력하기
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token(completion: { (token, error) in
+            guard let token = token else {
+                print("Token 에러: \(error.debugDescription)")
+                return
+            }
+            print("Token: \(token)")
+        })
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,7 +60,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -6,50 +6,9 @@
 //
 
 import UIKit
-import MapKit
-import CoreLocation
 
-class ViewController: UINavigationController, CLLocationManagerDelegate {
-    //지도 뷰
-    @IBOutlet var mapView:MKMapView!
-    //위치를 관리하는 객체 생성
-    var manager = CLLocationManager()
-    
+class ViewController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        manager.desiredAccuracy =  kCLLocationAccuracyBest //battery
-        manager.delegate = self
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
-    }
-    //locations에서 location을 받고, 매니저의 업데이트를 멈춤. location을 렌더링함.
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            manager.stopUpdatingLocation()
-            render(location)
-        }
-    }
-    //렌더링(지도에 좌표, 핀 등을 표시하기) 함수
-    func render(_ location:CLLocation) {
-        //좌표 값 객체 생성
-        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        //좌표 span 생성
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        //좌표 지역 객체 생성
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        //좌표 지역을 mapkit의 현재 표시될 지역으로 지정
-        mapView.setRegion(region, animated: true)
-        //핀 객체 생성
-        let pin = MKPointAnnotation()
-        //핀의 좌표 값을 위에 생성한 좌표 값으로 지정
-        pin.coordinate = coordinate
-        //핀 제목, 부제목 등
-        pin.title = "I'm here"
-        pin.subtitle = "There is my coordinate bro~~"
-        //지도에 핀을 추가
-        mapView.addAnnotation(pin)
     }
 }
