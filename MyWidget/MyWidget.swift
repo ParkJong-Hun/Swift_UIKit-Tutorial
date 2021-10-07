b5f6382660a1b90635bba4840223032efb53a372
ParkJong-Hun <thesn515@gmail.com>
2021-10-07 19:44:58 +0900

    2021/10/07 19:44 Widgets finish

--- MyWidget/MyWidget.swift
+++ MyWidget/MyWidget.swift
@@ -11,22 +11,32 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), text: "", configuration: ConfigurationIntent())
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(),
+                                text: "",
+                                configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        //appgroups에서 값을 받아와서 다른 앱(위젯이 아닌 실제 앱)에서 전달한 내용을 가져온다.
+        let userDefaults = UserDefaults(suiteName: "group.storyboardtutorialwidgetscache")
+        //가져온 데이터를 이용해 내용을 받는 변수 정의.
+        let text = userDefaults?.value(forKey: "text") as? String ?? "No Text"
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            //전달받은 내용을 이용한 텍스트라는 변수를 받는 객체 생성
+            let entry = SimpleEntry(date: entryDate,
+                                    text: text,
+                                    configuration: configuration)
+            //객체를 큐에 추가
             entries.append(entry)
         }
 
@@ -34,9 +44,10 @@ struct Provider: IntentTimelineProvider {
         completion(timeline)
     }
 }
-
+//실제로 사용될 여러 데이터를 저장
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let text: String
     let configuration: ConfigurationIntent
 }
 
@@ -44,7 +55,9 @@ struct MyWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
+        //실제 보여지는 부분 작성
         ZStack {
+            //이미지를 자체 크기, 좌표 공간의 함수로 정의
             GeometryReader { geo in
                 Image("background")
                     .resizable()
@@ -53,8 +66,9 @@ struct MyWidgetEntryView : View {
                            height: geo.size.height,
                            alignment: .center)
             }
-            Text(entry.date, style: .time)
-                .font(Font.system(size: 24, weight: .bold, design: .default))
+            //텍스트
+            Text(entry.text)
+                .font(Font.system(size: 24, weight: .bold, design: .default))//폰트 크기, 굵기, 생김새를 결정
                 .foregroundColor(Color.black)
         }
     }
@@ -63,19 +77,21 @@ struct MyWidgetEntryView : View {
 @main
 struct MyWidget: Widget {
     let kind: String = "MyWidget"
-
+    //위젯 표시
     var body: some WidgetConfiguration {
+        //intentdefinition 파일을 생성한다.
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            //위젯의 UI를 표현되는 실제 바디 함수
             MyWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("My Widget")//위젯의 이름을 표시
+        .description("This is an example widget.")//위젯의 설명을 표시
     }
 }
 
 struct MyWidget_Previews: PreviewProvider {
     static var previews: some View {
-        MyWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        MyWidgetEntryView(entry: SimpleEntry(date: Date(), text: "Hello Widget!", configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -6,16 +6,17 @@
 //
 
 import UIKit
+import WidgetKit
 
 class ViewController: UIViewController {
-    
+    //UITextField를 동적 생성
     private let field:UITextField = {
         let field = UITextField()
         field.placeholder = "Enter Text"
         field.backgroundColor = .white
         return field
     }()
-    
+    //UIButton을 동적 생성
     private let button:UIButton = {
         let button = UIButton()
         button.backgroundColor = .systemGreen
@@ -25,26 +26,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //뷰(컨테이너)의 배경색을 지정
         view.backgroundColor = .systemBlue
+        //뷰에 만든 필드, 버튼을 추가
         view.addSubview(field)
         view.addSubview(button)
+        //해당 뷰에서 객체를 firstResponder로 설정
         field.becomeFirstResponder()
-        
+        //버튼을 클릭하면 전달되어지는 목적지 설정
+        //해당 버튼을 클릭시 didTapButton 함수를 실행
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
+    //서브 뷰가 보여질 때
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        //필드의 너비, 높이 설정
         field.frame = CGRect(x: 20, y: view.safeAreaInsets.top+10,
                              width: view.frame.size.width-40,
                              height: 52)
-        
+        //버튼의 너비, 높이 설정
         button.frame = CGRect(x:30, y: view.safeAreaInsets.top+70,
                               width: view.frame.size.width-60,
                               height: 50)
     }
-    
+    //버튼을 눌렀을 때 appgroups를 통해 데이터가 전달됨.
     @objc private func didTapButton() {
+        field.resignFirstResponder()
         
+        let userDefaults = UserDefaults(suiteName: "group.storyboardtutorialwidgetscache")
+        
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        userDefaults?.setValue(text, forKey: "text")
+        //위젯의 타임라인을 새로고침
+        WidgetCenter.shared.reloadAllTimelines()
     }
 }
