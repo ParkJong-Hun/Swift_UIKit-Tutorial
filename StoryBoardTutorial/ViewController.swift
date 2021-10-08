f34cfb7e357e48a3f12054615da1b62b705d784a
ParkJong-Hun <thesn515@gmail.com>
2021-10-08 14:57:47 +0900

    2021/10/08 14:57 UseDefaults

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -6,62 +6,42 @@
 //
 
 import UIKit
-import WidgetKit
 
-class ViewController: UIViewController {
-    //UITextField를 동적 생성
-    private let field:UITextField = {
-        let field = UITextField()
-        field.placeholder = "Enter Text"
-        field.backgroundColor = .white
-        return field
-    }()
-    //UIButton을 동적 생성
-    private let button:UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemGreen
-        button.setTitle("Save", for: .normal)
-        return button
-    }()
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: User Defaults
+    /*
+     유저가 앱에서 무언가 활동을 하고 저장을 하였을 때, 앱을 종료하고 다시 열어도,
+     기존의 저장한 데이터를 사용할 수 있게 하는 것.
+     xcode에 기본 내장되어 있기 때문에 추가적인 라이브러리 필요 없음.
+     */
+    @IBOutlet var label: UILabel!
+    @IBOutlet var field: UITextField!
     
+    //UseDefaults 객체 생성
+    //let useDefaults = UserDefaults()
+    //안전하게 식별하기 위해 번들 아이디를 지정해 해당 번들의 UseDefaults 객체 생성
+    //let useDefaults = UserDefaults(suiteName: "[BundleID]")
+    //static 객체생성 class, suiteName을 혹은, UseDefaults.standard를 이용해 다른 클래스에서도 사용할 수 있는 싱글톤(shared) usedefaults도 만들 수 있음.
+    
+    //뷰가 로드될 때
     override func viewDidLoad() {
         super.viewDidLoad()
-        //뷰(컨테이너)의 배경색을 지정
-        view.backgroundColor = .systemBlue
-        //뷰에 만든 필드, 버튼을 추가
-        view.addSubview(field)
-        view.addSubview(button)
-        //해당 뷰에서 객체를 firstResponder로 설정
-        field.becomeFirstResponder()
-        //버튼을 클릭하면 전달되어지는 목적지 설정
-        //해당 버튼을 클릭시 didTapButton 함수를 실행
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        field.delegate = self
+        //userActivity에 savedText라는 키 이름으로 String 값이 저장되어 있다면 savedValue라는 변수를 선언하고 값을 넣음.
+        //if let savedValue = userActivity?.value(forKey: "savedText") as? String {
+        if let savedValue = UserDefaults.standard.string(forKey: "savedText") {
+            //레이블의 텍스트를 savedValue의 값으로 변경
+            label.text = savedValue
+        }
     }
     
-    //서브 뷰가 보여질 때
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        //필드의 너비, 높이 설정
-        field.frame = CGRect(x: 20, y: view.safeAreaInsets.top+10,
-                             width: view.frame.size.width-40,
-                             height: 52)
-        //버튼의 너비, 높이 설정
-        button.frame = CGRect(x:30, y: view.safeAreaInsets.top+70,
-                              width: view.frame.size.width-60,
-                              height: 50)
-    }
-    //버튼을 눌렀을 때 appgroups를 통해 데이터가 전달됨.
-    @objc private func didTapButton() {
+    //엔터키 같이 Return 역할을 하는 것을 입력햇을 때 실행
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //useDefaults에 article이라는 키 이름으로 field.text의 값을 저장.
+        //useDefaults.setValue(field.text, forKey: "savedText")
+        UserDefaults.standard.set(field.text, forKey: "savedText")
+        //첫번째 요청 상태를 포기(컴퓨터가 사람이 엔터를 한 번 눌러도 여러번 누른 것으로 인식해 여러번 저장하는 것을 방지)
         field.resignFirstResponder()
-        
-        let userDefaults = UserDefaults(suiteName: "group.storyboardtutorialwidgetscache")
-        
-        guard let text = field.text, !text.isEmpty else {
-            return
-        }
-        
-        userDefaults?.setValue(text, forKey: "text")
-        //위젯의 타임라인을 새로고침
-        WidgetCenter.shared.reloadAllTimelines()
+        return true
     }
 }
