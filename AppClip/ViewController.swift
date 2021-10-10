6a4b51abc45606086a5f4db406225a2dcd6d3123
ParkJong-Hun <thesn515@gmail.com>
2021-10-10 12:59:10 +0900

    2021/10/10 12:58 App Clips? I don't know about this function though

--- AppClip/ViewController.swift
+++ AppClip/ViewController.swift
@@ -1,14 +1,19 @@
 //
 //  ViewController.swift
-//  StoryBoardTutorial
+//  AppClip
 //
-//  Created by 박종훈 on 2021/08/10.
+//  Created by 박종훈 on 2021/10/10.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
+
+
 }
+
