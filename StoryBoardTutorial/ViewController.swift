cd0ffc75724de7d75e5dd5dfaadd2037bfc33c33
ParkJong-Hun <thesn515@gmail.com>
2021-10-10 11:50:50 +0900

    2021/10/10 11:50 Animation

--- StoryBoardTutorial/ViewController.swift
+++ StoryBoardTutorial/ViewController.swift
@@ -6,42 +6,56 @@
 //
 
 import UIKit
+//import TheAnimation animation의 keyPath가 무엇이 있는지 알 수 있고, 더 편하게 애니메이션을 만들 수 있는 라이브러리.
+//cocoapods로 설치해야한다.
 
-class ViewController: UIViewController, UITextFieldDelegate {
-    //MARK: User Defaults
-    /*
-     유저가 앱에서 무언가 활동을 하고 저장을 하였을 때, 앱을 종료하고 다시 열어도,
-     기존의 저장한 데이터를 사용할 수 있게 하는 것.
-     xcode에 기본 내장되어 있기 때문에 추가적인 라이브러리 필요 없음.
-     */
-    @IBOutlet var label: UILabel!
-    @IBOutlet var field: UITextField!
+class ViewController: UIViewController {
+    //MARK: Animation
+    //CALayer: CoreAnimation을 수행하는 이미지 기반 내용의 객체
+    let layer = CALayer()
     
-    //UseDefaults 객체 생성
-    //let useDefaults = UserDefaults()
-    //안전하게 식별하기 위해 번들 아이디를 지정해 해당 번들의 UseDefaults 객체 생성
-    //let useDefaults = UserDefaults(suiteName: "[BundleID]")
-    //static 객체생성 class, suiteName을 혹은, UseDefaults.standard를 이용해 다른 클래스에서도 사용할 수 있는 싱글톤(shared) usedefaults도 만들 수 있음.
-    
-    //뷰가 로드될 때
     override func viewDidLoad() {
         super.viewDidLoad()
-        field.delegate = self
-        //userActivity에 savedText라는 키 이름으로 String 값이 저장되어 있다면 savedValue라는 변수를 선언하고 값을 넣음.
-        //if let savedValue = userActivity?.value(forKey: "savedText") as? String {
-        if let savedValue = UserDefaults.standard.string(forKey: "savedText") {
-            //레이블의 텍스트를 savedValue의 값으로 변경
-            label.text = savedValue
-        }
+        layer.backgroundColor = UIColor.systemRed.cgColor
+        layer.frame = CGRect(x: 100, y: 100, width: 120, height: 120)
+        //view의 레이어의 서브 레이어로 추가
+        view.layer.addSublayer(layer)
+        //deadline 시간 이후에 execute의 내용을 실행하는 비동기
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            self.animateOpacity()
+        })
     }
-    
-    //엔터키 같이 Return 역할을 하는 것을 입력햇을 때 실행
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        //useDefaults에 article이라는 키 이름으로 field.text의 값을 저장.
-        //useDefaults.setValue(field.text, forKey: "savedText")
-        UserDefaults.standard.set(field.text, forKey: "savedText")
-        //첫번째 요청 상태를 포기(컴퓨터가 사람이 엔터를 한 번 눌러도 여러번 누른 것으로 인식해 여러번 저장하는 것을 방지)
-        field.resignFirstResponder()
-        return true
+    //레이어를 움직이게 하는 함수
+    func animateMovement() {
+        //position이라는 keyPath(이동)로 애니메이션 생성
+        let animation = CABasicAnimation(keyPath: "position")
+        //처음 값
+        animation.fromValue = CGPoint(x: layer.frame.origin.x + (layer.frame.width / 2),
+                                      y: layer.frame.origin.y + (layer.frame.height / 2))
+        //이후 값
+        animation.toValue = CGPoint(x: 300, y: 400)
+        //지속 시간
+        animation.duration = 1
+        //animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunction)
+        //애니메이션 모드
+        animation.fillMode = .forwards
+        //애니메이션이 끝난 후 없앨 것인지
+        animation.isRemovedOnCompletion = false
+        //시작 시간
+        animation.beginTime = CACurrentMediaTime()
+        //레이어에 애니메이션을 추가
+        layer.add(animation, forKey: nil)
+    }
+    //레이어를 투명하게 하는 함수
+    func animateOpacity() {
+        //opacity라는 keyPath(투명화)로 애니메이션 생성
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 1
+        animation.toValue = 0
+        animation.duration = 3
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        layer.add(animation, forKey: nil)
     }
 }
